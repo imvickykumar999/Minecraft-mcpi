a29ddac471fb380a2062a2b94eb7cab67ab08771
Vicky Kumar <imvickykumar999@gmail.com>
2024-01-22 11:29:57 +0530

    RaspberryJuice

--- raw files/coordinates.py
+++ raw files/coordinates.py
@@ -5,31 +5,44 @@
 from mcpi.minecraft import Minecraft
 
 # Initialize the api (MCPI must be open and in a world at this time)
-mc = Minecraft.create(address="127.0.0.1", port=58750)
+mc = Minecraft.create(address='localhost', port=4711)
 
 # Post to chat
 mc.postToChat("Hello world!")
 
-print(help(mc))
+# Get the player's position
+pos = mc.player.getTilePos()
+
+# Print the player's x,y,z
+print("X:", pos.x)
+print("Y:", pos.y)
+print("Z:", pos.z)
+
+# Get the player's rotation
+rot = mc.player.getRotation()
+
+# Print the player's roll, yaw and pitch
+print("Roll:", rot.roll)
+print("Yaw:", rot.yaw)
+print("Pitch:", rot.pitch)
+
+# We need only X, Z, and Yaw
+print(pos.x, pos.z, rot.yaw)
 
 
+rf'''
+C:\Users\surface\Documents\GitHub\Minecraft-mcpi\raw files>python coordinates.py
+Traceback (most recent call last):
+  File "C:\Users\surface\Documents\GitHub\Minecraft-mcpi\raw files\coordinates.py", line 45, in <module>
+    pos = mc.player.getTilePos()
+          ^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\surface\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\mcpi\minecraft.py", line 157, in getTilePos
+    return CmdPositioner.getTilePos(self, [])
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\surface\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\site-packages\mcpi\minecraft.py", line 56, in getTilePos
+    return Vec3(*list(map(int, s.split(","))))
+                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ValueError: invalid literal for int() with base 10: ''
 
-
-# # Get the player's position
-# pos = mc.player.getTilePos()
-
-# # Print the player's x,y,z
-# print("X:", pos.x)
-# print("Y:", pos.y)
-# print("Z:", pos.z)
-
-# # Get the player's rotation
-# rot = mc.player.getRotation()
-
-# # Print the player's roll, yaw and pitch
-# print("Roll:", rot.roll)
-# print("Yaw:", rot.yaw)
-# print("Pitch:", rot.pitch)
-
-# # We need only X, Z, and Yaw
-# print(pos.x, pos.z, rot.yaw)
+C:\Users\surface\Documents\GitHub\Minecraft-mcpi\raw files>
+'''
